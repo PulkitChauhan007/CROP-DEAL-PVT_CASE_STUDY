68040f9875ec826fa3ae3a0eb6f71c31fa0cb97c
Pulkit Chauhan <98642641+PulkitChauhan007@users.noreply.github.com>
2022-06-02 00:13:36 +0530

    Delete BACK-END directory

--- BACK-END/AdminService/src/main/java/com/casestudy/AdminService/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.casestudy.AdminService.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.casestudy.AdminService.models.Admin;
-
-public interface AdminRepository extends MongoRepository<Admin, String> {
-
-}
